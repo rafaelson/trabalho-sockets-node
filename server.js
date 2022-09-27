68bcc808a3aabbced5afb69c2cf26df67b5fd58a
Rafael Nelson <rafaelson1337@gmail.com>
2022-09-27 10:01:02 -0300

    translated some things to portuguese

--- server.js
+++ server.js
@@ -139,6 +139,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(3000, "localhost", () => {
+  console.log("escutando em localhost:3000");
 });
